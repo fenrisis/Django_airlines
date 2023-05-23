c1cc4935d60b08df5fdfbd1c145483b3e729c814
Leonid3# <116304748+fenrisis@users.noreply.github.com>
2023-05-23 03:19:52 +0200

    3rd commit
    -Customize many to many relationships
    -Added a new class Passenger & preferences
    -Now you can add a passenger to a flight
    -created a links trought pages
    -new option to book a flight

--- flights/admin.py
+++ flights/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 
-from .models import Flight, Airport
+from .models import Airport, Flight, Passenger
 
 # Register your models here.
+
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "duration")
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights",)
+    
+
 admin.site.register(Airport)
-admin.site.register(Flight)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
